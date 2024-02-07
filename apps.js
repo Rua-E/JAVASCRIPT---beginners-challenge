742512cdfcf33d68d26fdb05b92b235d7b80d423
Rua <rua.elkasheef@gmail.com>
2024-02-07 15:31:28 -0500

    practicing functions and adding

--- apps.js
+++ apps.js
@@ -1,1 +1,24 @@
 console.log('it wroked')
+
+function addition(num1, num2) {
+    return (num1 + num2)
+}
+console.log(addition(3,5));
+console.log(addition(-3,-6));
+console.log(addition(7,3));
+
+function hoursIntoSeconds(hours) {
+    return hours *3600
+}
+console.log(hoursIntoSeconds (2));
+console.log (hoursIntoSeconds(10));
+console.log(hoursIntoSeconds(24));
+
+
+function calcParimeter(length, width) {
+    return(length*2 + width*2)
+}
+
+console.log(calcParimeter(6,7));
+console.log(calcParimeter(20,10));
+console.log(calcParimeter(2,9));
